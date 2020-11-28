b0cb6b5abd1e3ee76fb2ebdf1709b4b0a5d58473
Maxr1998 <max.rumpf1998@gmail.com>
2020-11-28 18:32:21 +0100

    Convert VOBs to H.265 instead

--- terminal/.config/fish/functions/vobrip.fish
+++ terminal/.config/fish/functions/vobrip.fish
@@ -1,10 +1,10 @@
-function vobrip --description 'Transcodes a VOB to H.264'
+function vobrip --description 'Transcodes a VOB to H.265'
     if test (count $argv) -ne 1
         echo "Illegal use, requires a VOB file argument"
     end
 
     set input $argv[1]
-    set output (string split -r -m 1 . "$input")[1].mkv
+    set output (string split -r -m 1 . "$input")[1].out.mkv
     echo "Encoding $input to $output"
-    ffmpeg -i "$input" -vf yadif=1 -c:v libx264 -preset veryslow -crf 18 -c:a flac "$output"
+    ffmpeg -i "$input" -vf yadif=1 -c:v libx265 -preset veryslow -crf 18 -c:a flac "$output"
 end
