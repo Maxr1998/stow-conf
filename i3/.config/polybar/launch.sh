0112ec35c3cae648319d8fe0022875d9c31c9c10
Maxr1998 <max.rumpf1998@gmail.com>
2021-11-30 15:16:53 +0100

    Launch Polybar on all monitors

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar stool &
+# Launch Polybar on all monitors, using default config location ~/.config/polybar/config
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar stool &
+done
 
 echo "Polybar launched..."
