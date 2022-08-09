a1f8794fc515411008a5d722a0f36243251f9c51
Maxr1998 <max.rumpf1998@gmail.com>
2022-08-09 13:54:43 +0200

    Reformat setup.sh

--- setup.sh
+++ setup.sh
@@ -15,26 +15,26 @@ end
 set -l I3_DIR i3/.config/i3
 
 if count $argv > /dev/null
-    and test $argv[1] = "pkg"
-    which pacman > /dev/null
-    if test $status -eq 0
-        colorecho "Arch Linux detected, installing packages if required.."
-        sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-        sudo pacman -Syu
-        sudo pacman -S --needed (cat pkgs.txt)
-        echo
-        colorecho "Installing custom config package"
-        set stow_root "$PWD"
-        cd i3/.config/i3/arch_pkg/
-        makepkg -fci
-        cd $stow_root
-        echo
-        colorecho "Install manually afterwards:"
-        colorecho "- https://aur.archlinux.org/package-query.git"
-        colorecho "- https://aur.archlinux.org/yay.git"
-        colorecho "yay -S --needed --noconfirm polybar-git i3lock-color-git \\ "
-        colorecho "materia-gtk-theme numix-circle-icon-theme-git numix-icon-theme-git ttf-font-icons"
-        echo
+	and test $argv[1] = "pkg"
+	which pacman > /dev/null
+	if test $status -eq 0
+		colorecho "Arch Linux detected, installing packages if required.."
+		sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+		sudo pacman -Syu
+		sudo pacman -S --needed (cat pkgs.txt)
+		echo
+		colorecho "Installing custom config package"
+		set stow_root "$PWD"
+		cd i3/.config/i3/arch_pkg/
+		makepkg -fci
+		cd $stow_root
+		echo
+		colorecho "Install manually afterwards:"
+		colorecho "- https://aur.archlinux.org/package-query.git"
+		colorecho "- https://aur.archlinux.org/yay.git"
+		colorecho "yay -S --needed --noconfirm polybar-git i3lock-color-git \\ "
+		colorecho "materia-gtk-theme numix-circle-icon-theme-git numix-icon-theme-git ttf-font-icons"
+		echo
     end
     sleep 3
 end
@@ -48,24 +48,25 @@ mkdir -p ~/.config/jftui/
 
 colorecho "Checking for Vundle"
 if not test -d ~/.vim/bundle/Vundle.vim
-    colorecho "Installing.."
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	colorecho "Installing.."
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 end
 
 if count $argv > /dev/null
-   and [ $argv[1] = "teardown" ]
-    for pkg in */
-        set pkg (echo $dir | sed 's|/$||')
-        colorecho "CURRENTLY UNLINKING $pkg"
-        stow -D -t ~ "$pkg"
-    end
+	and [ $argv[1] = "teardown" ]
+	for pkg in */
+		set pkg (echo $dir | sed 's|/$||')
+		colorecho "CURRENTLY UNLINKING $pkg"
+		stow -D -t ~ "$pkg"
+	end
 else
-    for dir in $enabled_modules
-        colorecho "CURRENTLY LINKING $pkg"
-        stow -t ~ "$pkg"
-    end
+	for dir in $enabled_modules
+		colorecho "CURRENTLY LINKING $pkg"
+		stow -t ~ "$pkg"
+	end
 end
 
 colorecho "Patching GDM Xsession"
 sudo sed -i 's/xrdb -nocpp -merge "$userresources"/xrdb -merge "$userresources"/' /etc/gdm/Xsession
 
+# vim: noet
