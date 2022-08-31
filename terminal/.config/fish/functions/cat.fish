fa107f1f27d3e491b42781e7bf7aca6ec0db5516
Maxr1998 <max.rumpf1998@gmail.com>
2022-08-31 11:29:15 +0200

    Fall back to system cat if bat is unavailable

--- terminal/.config/fish/functions/cat.fish
+++ terminal/.config/fish/functions/cat.fish
@@ -1,8 +1,9 @@
-# Defined in - @ line 0
-function cat --description 'alias cat=bat'
-    if status --is-interactive
-        bat  $argv;
-    else
-        command cat $argv;
+if type -q bat
+    function cat --description 'alias cat=bat'
+        if status --is-interactive
+            bat  $argv;
+        else
+            command cat $argv;
+        end
     end
 end
