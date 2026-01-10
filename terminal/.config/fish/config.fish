a01eb311fa45d12ab339d79cd5bb58ff08e2647e
Maxr1998 <max.rumpf1998@gmail.com>
2026-01-10 15:28:15 +0100

    Tweak fish config

--- terminal/.config/fish/config.fish
+++ terminal/.config/fish/config.fish
@@ -19,34 +19,32 @@ set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/keyring/ssh
 set -g fish_key_bindings fish_vi_key_bindings
 set -g fish_cursor_insert block
 
+# cat
+set -gx BAT_PAGER ""
+
 # Zoxide
 if type -q zoxide
     zoxide init fish | source
 end
 
-# Python + Pyenv
+# Python keyring
 set -gx PYTHON_KEYRING_BACKEND keyring.backends.fail.Keyring
-
-if type -q pyenv
-    pyenv init - | source
-end
 
 ### Default programs ###
 
 set -gx EDITOR vim
 set -gx VISUAL vim
 
-set -gx BAT_PAGER ""
-
 ### User info ###
 
-export MPW_FULLNAME=Maxr1998
-
-set GITHUB_USER Maxr1998
+set -gx GITHUB_USER Maxr1998
+set -gx MPW_FULLNAME Maxr1998
 
 # Commands to run in interactive sessions can go here
 if status is-interactive
     complete -c svim -w vim
+
+    complete -c wget -l content-disposition -d "Use filename from server."
 
     if type -q fzf
         fzf_key_bindings
